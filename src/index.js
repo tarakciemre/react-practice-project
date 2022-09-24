2feae50913202dafd709df6907d0bb2f1e7a0c86
Emre Tarakci <tarakci.yazilim@gmail.com>
2022-09-24 16:47:09 +0300

    index css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+// CSS
+import './index.css'
+const books = [
+  {
+    id: 1,
+    img: 
+      'https://kbimages1-a.akamaihd.net/52c1eab8-f621-4f33-86e2-92f568001482/353/569/90/False/man-s-search-for-meaning.jpg',
+    title: "Man's Search for Meaning",
+    author: "Viktor Frankl",
+  },
+  {
+    id: 2,
+    img: 
+      'https://kbimages1-a.akamaihd.net/c9472126-7f96-402d-ba57-5ba4c0f4b238/353/569/90/False/nineteen-eighty-four-1984-george.jpg',
+    title: "1984",
+    author: "George Orwell",
+  },
+  {
+    id: 3,
+    img: 
+      'https://kbimages1-a.akamaihd.net/c9472126-7f96-402d-ba57-5ba4c0f4b238/353/569/90/False/nineteen-eighty-four-1984-george.jpg',
+    title: "Book Title",
+    author: "Emre Tarakcı",
+  },
+];
 
 // since this has a capital function name, React knows this is a component
-function Greeting() {
-  return <h4>This is Emre and this is my first component</h4>;
+function Booklist() {
+  return  (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Book = (props) => {
+  const {img, title, author, children} = props;
+  return (
+    <article className='book'>
+      <img src={img} alt="" width="100" height="100"></img> 
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  );
+}
+
+ReactDom.render(<Booklist />, document.getElementById('root'));
